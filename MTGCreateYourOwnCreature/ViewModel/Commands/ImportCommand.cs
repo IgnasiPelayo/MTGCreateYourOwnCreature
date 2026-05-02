c4bdf9353e46b32ff1753a5ba88e3797437cedce
Ignasi <ignasipelayo@gmail.com>
2026-05-02 16:06:39 +0200

    Initial Commit

---
+++ MTGCreateYourOwnCreature/ViewModel/Commands/ImportCommand.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MTGCreateYourOwnCreature.ViewModel.Commands
+{
+    internal class ImportCommand : ICommand
+    {
+        public MTGCreateYourOwnCreatureVM VM { get; set; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public ImportCommand(MTGCreateYourOwnCreatureVM vm)
+        {
+            VM = vm;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = true,
+                DefaultExt = ".txt",
+                Filter = "Text documents (.txt)|*.txt"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                VM.ImportFile(dialog.FileName);
+            }
+        }
+    }
+}
---
+++ MTGCreateYourOwnCreature/ViewModel/MTGCreateYourOwnCreatureVM.cs
@@ -0,0 +1,28 @@
+﻿using MTGCreateYourOwnCreature.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MTGCreateYourOwnCreature.ViewModel
+{
+    internal class MTGCreateYourOwnCreatureVM : INotifyPropertyChanged
+    {
+        public ImportCommand ImportCommand { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MTGCreateYourOwnCreatureVM()
+        {
+            ImportCommand = new ImportCommand(this);
+        }
+
+        public void ImportFile(string filePath)
+        {
+            System.Diagnostics.Debug.WriteLine("Importing...");
+        }
+    }
+}
