eafb7643081b3f5b712a344376f99372c0c93292
Ignasi Pelayo <ignasipelayo@gmail.com>
2026-05-09 15:58:43 +0200

    Created control for inspector items

---
+++ MTGCreateYourOwnCreature/View/Controls/CardNameInspectorControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MTGCreateYourOwnCreature.View.Controls
+{
+    public partial class CardNameInspectorControl : UserControl
+    {
+        public CardNameInspectorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
