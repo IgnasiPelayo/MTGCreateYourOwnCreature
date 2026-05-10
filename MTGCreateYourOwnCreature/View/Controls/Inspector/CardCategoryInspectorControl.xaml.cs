2a30a5bf84e0cca707c23f8db24e1e1b73bb3b25
Ignasi <ignasipelayo@gmail.com>
2026-05-10 14:08:57 +0200

    Added Category ComboBox Placeholder

---
+++ MTGCreateYourOwnCreature/View/Controls/Inspector/CardCategoryInspectorControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MTGCreateYourOwnCreature.View.Controls.Inspector
+{
+    public partial class CardCategoryInspectorControl : UserControl
+    {
+        public CardCategoryInspectorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
