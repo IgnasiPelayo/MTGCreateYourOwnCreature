6956f22cd27716bac7793e58631ef8f07bf11f83
Ignasi Pelayo <ignasipelayo@gmail.com>
2026-06-01 00:52:08 +0200

    Started Card Preview

---
+++ MTGCreateYourOwnCreature/View/Controls/Preview/CardPreviewFrameControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace MTGCreateYourOwnCreature.View.Controls.Preview
+{
+    public partial class CardPreviewFrameControl : UserControl
+    {
+        public CardPreviewFrameControl()
+        {
+            InitializeComponent();
+        }
+
+        public Brush Fill
+        {
+            get => (Brush)GetValue(FillProperty);
+            set => SetValue(FillProperty, value);
+        }
+
+        public static readonly DependencyProperty FillProperty = DependencyProperty.Register(
+            nameof(Fill), typeof(Brush), typeof(CardPreviewFrameControl));
+    }
+}
